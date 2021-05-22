39ae1322e4ffa2d626a94877db0bb796f68e2394
Jakob Bourhil <jakob@genity.se>
2021-05-22 14:20:27 +0200

    Task app done, and added json-server as mock server

---
+++ src/components/Button.js
@@ -0,0 +1,19 @@
+export const Button = ({ color, text, onClick }) => {
+  return (
+    <button 
+          onClick={onClick}
+          style={{ backgroundColor: color }} 
+          className='btn'
+        >
+          {text}
+        </button>
+  )
+
+}
+
+Button.defaultProps = {
+  color: 'black',
+  text: 'Button'
+}
+
+export default Button
